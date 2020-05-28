93eadfe1130ea61c4ec0de5d69ceddc5b766e834
Akash Giri <akashgiri27aug@gmail.com>
2020-05-28 10:27:26 +0530

    Redesigned plan 1

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -20,73 +20,76 @@ public class YourService extends KiboRpcService {
         // write here your plan 1
 
         api.judgeSendStart();
+
         // move Robot from the starting point to P1-1
-        Point point = new Point(1.1, -2.2, 3.3);
-        Quaternion quaternion = new Quaternion(4.4f, -5.5f, 6.6f, -7.7f);
+        Point point = new Point(11.5, -5.7, 4.5);
+        Quaternion quaternion = new Quaternion(0,0,0,1);
         api.moveTo(point, quaternion, true);
-
         Bitmap snapshot = api.getBitmapNavCam();
-
-
         String valueX = String.valueOf(point.getX());
         api.judgeSendDiscoveredQR(0, valueX);
-
         String markerId = snapshot.toString();
-
-        api.laserControl(true);
         sendData(MessageType.JSON, "data", "SUCCESS:defaultapk "+markerId);
+
+        // move Robot from the starting point to P1-2
+        point = new Point(11,-6,5.55);
+        quaternion = new Quaternion(0.0f,-0.7071068f,0.0f,0.7071068f);
+        api.moveTo(point, quaternion, true);
+        snapshot = api.getBitmapNavCam();
+        valueX = String.valueOf(point.getX());
+        api.judgeSendDiscoveredQR(0, valueX);
+        markerId = snapshot.toString();
+        sendData(MessageType.JSON, "data", "SUCCESS:defaultapk "+markerId);
+
+        // move Robot from the starting point to P1-3
+        point = new Point(11, -5.5, 4.33);
+        quaternion = new Quaternion(0,0,1,0);
+        api.moveTo(point, quaternion, true);
+        snapshot = api.getBitmapNavCam();
+        valueX = String.valueOf(point.getX());
+        api.judgeSendDiscoveredQR(0, valueX);
+        markerId = snapshot.toString();
+        sendData(MessageType.JSON, "data", "SUCCESS:defaultapk "+markerId);
+
+        // move Robot from the starting point to P2-1
+        point = new Point(11.5,-8,5);
+        quaternion = new Quaternion(0,0,0,1);
+        api.moveTo(point, quaternion, true);
+        snapshot = api.getBitmapNavCam();
+        valueX = String.valueOf(point.getX());
+        api.judgeSendDiscoveredQR(0, valueX);
+        markerId = snapshot.toString();
+        sendData(MessageType.JSON, "data", "SUCCESS:defaultapk "+markerId);
+
+
+        // move Robot from the starting point to P2-2
+        point = new Point(11,-7.7,5.55);
+        quaternion = new Quaternion(4.4f, -5.5f, 6.6f, -7.7f);
+        api.moveTo(point, quaternion, true);
+        snapshot = api.getBitmapNavCam();
+        valueX = String.valueOf(point.getX());
+        api.judgeSendDiscoveredQR(0, valueX);
+        markerId = snapshot.toString();
+        sendData(MessageType.JSON, "data", "SUCCESS:defaultapk "+markerId);
+
+        // move Robot from the starting point to P2-3
+        point = new Point(1.1, -2.2, 3.3);
+        quaternion = new Quaternion(0.0f,-0.7071068f,0.0f,0.7071068f);
+        api.moveTo(point, quaternion, true);
+        snapshot = api.getBitmapNavCam();
+        valueX = String.valueOf(point.getX());
+        api.judgeSendDiscoveredQR(0, valueX);
+        api.laserControl(true);
+        markerId = snapshot.toString();
+        sendData(MessageType.JSON, "data", "SUCCESS:defaultapk "+markerId);
+
+
 
     }
 
     @Override
     public void sendData(MessageType type, String topic,String data) {
         super.sendData(type, topic, data);
-    }
-
-    @Override
-    protected void runPlan2(){
-        // write here your plan 2
-
-        api.judgeSendStart();
-        // move Robot from the starting point to P1-1
-        Point point = new Point(1.1, -2.2, 3.3);
-        Quaternion quaternion = new Quaternion(4.4f, -5.5f, 6.6f, -7.7f);
-        api.moveTo(point, quaternion, true);
-
-        Bitmap snapshot = api.getBitmapNavCam();
-
-
-        String valueX = String.valueOf(point.getX());
-        api.judgeSendDiscoveredQR(0, valueX);
-
-        String markerId = snapshot.toString();
-
-        api.laserControl(true);
-        sendData(MessageType.JSON, "data", "SUCCESS:defaultapk "+markerId);
-
-    }
-
-    @Override
-    protected void runPlan3(){
-        // write here your plan 3
-
-        api.judgeSendStart();
-        // move Robot from the starting point to P1-1
-        Point point = new Point(1.3, -2.1, 3.1);
-        Quaternion quaternion = new Quaternion(4.4f, -5.5f, 6.6f, -7.7f);
-        api.moveTo(point, quaternion, true);
-
-        Bitmap snapshot = api.getBitmapNavCam();
-
-
-        String valueX = String.valueOf(point.getX());
-        api.judgeSendDiscoveredQR(0, valueX);
-
-        String markerId = snapshot.toString();
-
-        api.laserControl(true);
-        sendData(MessageType.JSON, "data", "SUCCESS:defaultapk "+markerId);
-
     }
 
 }
